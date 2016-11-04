445a16f6d6ea0e4c45b8c1cf64016c69d1775912
Puneet Singh Bagga <student@macbook-pro-441-bagga.local>
2016-11-04 09:24:39 -0400

    No more SIGABRT error which was caused by errors with allowing access to pictures on the iPhone. It now works fully. The rating control is in the process of being added, right now in place of the stars is a red square.

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,32 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Puneet Singh Bagga on 2016-11-04.
+//  Copyright © 2016 Puneet Singh Bagga. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    //MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.red
+        addSubview(button)
+    }
+    
+    override public var intrinsicContentSize: CGSize {
+        
+        get{
+            //return intrinsic content size for the stack view
+            return CGSize(width: 240, height: 44)
+            
+        }
+        
+    }
+    
+}
+
