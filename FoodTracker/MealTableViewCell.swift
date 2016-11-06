104fc0387552d16973949a508175e399cf259111
rsgc-bagga-p <pbagga@rsgc.on.ca>
2016-11-06 13:50:14 -0500

    Added the image view, label and rating to the table view but nothing is shown yet. Just the objects have been added.

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Puneet Singh Bagga on 2016-11-06.
+//  Copyright © 2016 Puneet Singh Bagga. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
