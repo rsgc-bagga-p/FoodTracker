188c0d0fa3e1ce1b535ccf481bb07bd5234889ec
rsgc-bagga-p <pbagga@rsgc.on.ca>
2016-11-06 18:28:08 -0500

    If save is clicked, the changes made will overwrite the existing meal in the list.

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -138,17 +138,25 @@ class MealTableViewController: UITableViewController {
         }
     }
     
+
     @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
         
         if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
             
-            // Add a new meal.
-            let newIndexPath = IndexPath(row: meals.count, section: 0)
-            meals.append(meal)
-            tableView.insertRows(at: [newIndexPath], with: .bottom)
-            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                
+                meals[selectedIndexPath.row] = meal
+                
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+                
+            } else {
+                
+                let newIndexPath = IndexPath(row: meals.count, section: 0)
+                
+                meals.append(meal)
+                
+                tableView.insertRows(at: [newIndexPath], with: .bottom)
+            }
         }
-        
-    }
-
+}
 }
